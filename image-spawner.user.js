521ebfb8389cbb55b35170b8289d34de35eea6e1
kittenzflight <w8163230@gmail.com>
2025-12-11 22:19:20 -0500

    Update image-spawner.user.js

--- image-spawner.user.js
+++ image-spawner.user.js
@@ -99,7 +99,7 @@
             img.style.transition = "opacity 400ms linear";
             document.body.appendChild(img);
             // trigger fade-in to 50% opacity (semi-transparent)
-            requestAnimationFrame(() => { img.style.opacity = "0.5"; });
+            requestAnimationFrame(() => { img.style.opacity = "0.0"; });
         }
         // spawn every 500ms forever
         setInterval(spawnImage, 4000);
