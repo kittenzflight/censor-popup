2cbc8316d2d89e7b62be8471bad3b856a3516ad8
kittenzflight <w8163230@gmail.com>
2025-12-12 00:59:52 -0500

    Update image-spawner.user.js

--- image-spawner.user.js
+++ image-spawner.user.js
@@ -126,10 +126,10 @@
             setTimeout(() => {
                 img.style.opacity = "0";
                 setTimeout(() => img.remove(), 400); // Remove after fade-out completes
-            }, 1000);
+            }, 3000);
         }
         // spawn every 5000ms (5 seconds) forever
-        setInterval(spawnImage, 500);
+        setInterval(spawnImage, 1000);
     }
     
     // Ensure document.body exists before running (fixes errors on about:blank)
